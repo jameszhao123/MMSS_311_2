6c1bb99d8cb9f3fc65e13cfabed3a5a1332e651b
jameszhao123 <jameszhao2021@u.northwestern.edu>
2019-04-12 00:41:33 -0500

    Further work on HW0 Q2

--- HW0 Code Q2.R
+++ HW0 Code Q2.R
@@ -25,3 +25,20 @@ pums_chicago$SERIALNO <- NULL
 write.csv(pums_chicago,'editedPUMS_CHICAGO.csv')
 
 #2h Use the variable ESR, create 5 new dataframes: under 16, employed, unemployed, in the armed forces, and not in the labor force. 
+#2i Create a new dataframe that combines employed people and people in the armed forces. 
+#2j In your new employed_af dataframe, keep only the variables AGEP, RAC1P, and PINCP_LOG
+
+#2ki Find the mean, median, and 80th percentile of travel time to work, JWMNP 
+summary(pums_chicago$JWMNP)
+quantile(pums_chicago$JWMNP, probs=0.8, na.rm=TRUE)
+
+#2kii Find the correlation between travel time to work JWMNP and annual wages WAGP 
+cor(pums_chicago$JWMNP, pums_chicago$WAGP, use="complete.obs")
+
+#2kiii Make a scatterplot of age and log income.
+#2kiv Export this graph to your working directory in pdf format.
+pdf("ageonLogincome.pdf")
+plot(pums_chicago$AGEP, pums_chicago$PINCP_LOG, main="age on log income")
+dev.off()
+
+
