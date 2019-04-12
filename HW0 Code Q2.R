0901acc0d48a092dd103479aec6e33cc03314ea9
jameszhao123 <jameszhao2021@u.northwestern.edu>
2019-04-11 23:16:32 -0500

    Further work on HW0 Q2, up to section k

--- HW0 Code Q2.R
+++ HW0 Code Q2.R
@@ -9,5 +9,19 @@ pums_chicago <- read.csv("pums_chicago.csv")
 #2c What is the mean annual income, PINCP in this dataset? (why wrong?) (why can't save file?)
 PINCP_mean <- mean("PINCP", na.rm = TRUE)
 
-#2d (d) Create a new variable in the PUMS dataframe called PINCP_LOG that is equal to the log of annual income. Were NaN values produced? Why?
+#2d Create a new variable in the PUMS dataframe called PINCP_LOG that is equal to the log of annual income. Were NaN values produced? Why?
+#unsure!
+#NaN values were produced because we cannot take log of negative numbers.
 
+pums_chicago$PINCP_LOG <- log(pums_chicago$PINCP)
+
+#2e Create a new variable GRAD.DUMMY that takes the value "grad" if the respondent has any post-high school education, and "no grad" otherwise. Use the SCHL variable. 
+pums_chicago$GRAD.DUMMY <- ifelse(pums_chicago$SCHL > 12, "grad", "no grad")
+
+#2f Drop the variable SERIALNO from the dataset.
+pums_chicago$SERIALNO <- NULL
+
+#2g Save your new dataset to a csv ???le in the working directory.
+write.csv(pums_chicago,'editedPUMS_CHICAGO.csv')
+
+#2h Use the variable ESR, create 5 new dataframes: under 16, employed, unemployed, in the armed forces, and not in the labor force. 
