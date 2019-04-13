eb19d3605097a6163104ea960111592e7f69691a
jameszhao123 <jameszhao2021@u.northwestern.edu>
2019-04-12 23:10:46 -0500

    Completed HW0; need to check

--- HW0 Code Q2.R
+++ HW0 Code Q2.R
@@ -24,17 +24,17 @@ pums_chicago$SERIALNO <- NULL
 write.csv(pums_chicago,'editedPUMS_CHICAGO.csv')
 
 #2h Use the variable ESR, create 5 new dataframes: under 16, employed, unemployed, in the armed forces, and not in the labor force.
-#something is WRONG HERE!!! total does not add up to 50K Observations
 under16 <- pums_chicago[pums_chicago$ESR == "NA", ]
-employed <- pums_chicago[pums_chicago$ESR == 1 & pums_chicago$ESR == 2, ]
-unemployed <- pums_chicago[pums_chicago$ESR == 3, ]
-armedforces <- pums_chicago[pums_chicago$ESR == 4 & pums_chicago$ESR == 5, ]
-notinlaborforce <- pums_chicago[pums_chicago$ESR == 6, ]
+employed <- pums_chicago[pums_chicago$ESR %in% c("1", "2"), ]
+unemployed <- pums_chicago[pums_chicago$ESR == "3", ]
+armedforces <- pums_chicago[pums_chicago$ESR %in% c("4", "5"), ]
+notinlaborforce <- pums_chicago[pums_chicago$ESR == "6", ]
 
 #2i Create a new dataframe that combines employed people and people in the armed forces. 
-employed_af <-
+employed_af <- pums_chicago[pums_chicago$ESR %in% c("1", "2", "4", "5"), ]
 
 #2j In your new employed_af dataframe, keep only the variables AGEP, RAC1P, and PINCP_LOG
+new_employed_af <- pums_chicago[c("AGEP", "RAC1P", "PINCP_LOG")]
 
 #2ki Find the mean, median, and 80th percentile of travel time to work, JWMNP 
 summary(pums_chicago$JWMNP)
@@ -49,10 +49,47 @@ pdf("ageonLogincome.pdf")
 plot(pums_chicago$AGEP, pums_chicago$PINCP_LOG, main="age on log income")
 dev.off()
 
+#2kv Create a crosstab of employment status ESR by race RAC1P
+install.packages("gmodels")
+library("gmodels")
+CrossTable(pums_chicago$ESR, pums_chicago$RAC1P)
 
-#2li
+#2kvi Estimate a linear regression of annual wages WAGP on hours worked per week WKHP 
+wagp_on_wkhp <- lm(WAGP ~ WKHP, pums_chicago)
+
+#2kvii Plot the residuals from this regression against the ???tted values. What does this show?
+#This shows that
+wagp_on_wkhp_res <- resid(wagp_on_wkhp)
+wagp_on_wkhp_fitted <- fitted(wagp_on_wkhp)
+plot(wagp_on_wkhp_fitted, wagp_on_wkhp_res, 
+     ylab = "Residuals", xlab="Fitted",
+     main = "Residuals on Fitted")
+
+#2li Estimate a linear regression of miles per gallon on weight 
 data(mtcars)
-
 car_lm <- lm(mpg ~ wt, mtcars)
+
+#2lii Estimate this regression separately for manual versus automatic transition 
+autoData <- mtcars[mtcars$am == "0",]
+manualData <- mtcars[mtcars$am == "1",]
+autocar_lm <- lm(mpg ~ wt, mtcars)
+manualcar_lm <- lm(mpg ~ wt, mtcars)
+
+#2liii Estimate a regression of miles per gallon on the log of horsepower. 
+mtcars$log.hp <- log(mtcars$hp)
+mpg_on_lg.hp <- lm(mpg ~ log.hp, mtcars)
+
+#2mi Make a scatterplot of weight against miles per gallon. 
+#2mii Color the points in your graph according to the transmission of the vehicle. 
+#2miii Change the shape of the points to correspond to the number of forward gears in the vehicle.
+#2miv Change the x and y labels on the plot to make full words. 
+#2mv Change the background of the plot so that the panel background is not gray.
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(mtcars)+ geom_point(mapping = aes(x = mpg, y = wt, 
+                                         color = mtcars$am, 
+                                         shape = mtcars$gear)) + scale_shape_identity() + 
+  labs(title = "Weight on Miles per Gallon", x = "Miles per Gallon", y = "Weight") +
+  theme(panel.background = element_rect(fill = "lightyellow"))
 
 
